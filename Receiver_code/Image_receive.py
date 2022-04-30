3dd6d0f58fb468857e88aa2ee9a5dbcc073f4fdc
Vistasp Edulji <31438241+The-VAE23@users.noreply.github.com>
2022-04-30 14:16:29 +0530

    Update Image_receive.py
    Added code to send/receive at px/cycle

--- Receiver_code/Image_receive.py
+++ Receiver_code/Image_receive.py
@@ -25,23 +25,25 @@ def logimg():
     """Function that acquires img from the remote device"""
     # Variables to identify image shape
     imsize = (102240,230400,409920,921600,2073600,307200,480000,691200,786432,1228800,1470000,1555200,24300)
-    imshape = ((240,426),(360,640),(480,854),(720,1280),(1080,1920),(480,640),(600,800),(720,960),(768,1024),(960,1280),(1050,1400),(1080,1440),(180,135))
+    imshape = ((240,426),(360,640),(480,854),(720,1280),(1080,1920),(480,640),(800,600),(720,960),(768,1024),(960,1280),(1050,1400),(1440,1080),(180,135))
     img = []
     while True:
         x=AR.inWaiting()
         if x>0:
-            data = readSerial(AR).decode('utf-8')
-            if data[0:-2] == "e":
+            data = readSerial(AR).decode('utf-8').split(',')
+            if data[0][0:-2] == "e":
                 break
             else:
-                img.append(int(data))
+                for x in data:
+                    img.append(int(x))
     img = np.array(img)
     i = imsize.index(img.size)
     img = img.reshape(imshape[i])
-    img = img.astype(np.uint8)
+    # img = img.astype(np.uint8) #Uncomment if using HSV or other float based image datasets
     time_utc = datetime.now()
     time_utc = str(time_utc)[0:-7].replace(':',"").replace('-',"").replace(' ',"")
-    cv2.imwrite("D:Data/"+time_utc+'.png', img)
+    cv2.imwrite("D:/College/Data/LYpro/"+time_utc+'.png', img)
+    # plt.imshow(img,'gray')   #Uncomment line for output in code
     print("Image saved")
 
 # Looping function that checks for available image and saves it
